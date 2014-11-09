599f4bb7c03adb0918ce37e5f5e56f91c70cba59
Akshay Bajaj <akshaybajaj2280@gmail.com>
2014-11-09 14:29:41 -0600

    added stuff to case where user doesn't share location

--- js/geolocation.js
+++ js/geolocation.js
@@ -8,28 +8,31 @@ if (navigator.geolocation)
     // argument - our configuration options - are optional.
     navigator.geolocation.getCurrentPosition
     (
+        console.log("Attempting to get user's location from browser ");
         function( position )
         {
-                // alert( 'lat: ' + position.coords.latitude + ' long:' + position.coords.longitude );
-                var google_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + ',' + position.coords.longitude;
-                console.log("Getting location at: " + google_url);
-                $.getJSON(google_url,
-                    function(data) {
-                        user_location = data.results[1]['formatted_address']
-                        console.log("Location determined: " + user_location);
-                        var location_div=document.getElementById('location');
-                        location_div.innerHTML="<h3>Looks like you're in " + user_location + "</h3>";
-                    }
-                );
+            // alert( 'lat: ' + position.coords.latitude + ' long:' + position.coords.longitude );
+            var google_url = "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + position.coords.latitude + ',' + position.coords.longitude;
+            console.log("Getting location at: " + google_url);
+            $.getJSON(google_url,
+                function(data) {
+                    user_location = data.results[1]['formatted_address']
+                    console.log("Location determined: " + user_location);
+                    var location_div=document.getElementById('location');
+                    location_div.innerHTML="<h3>Looks like you're in " + user_location + "</h3>";
+                }
+            );
 
         },
         function( error ){
-                console.log( "Something went wrong: ", error );
+            console.log("User refused to share location or there was an error: ", error);
+            var location_div=document.getElementById('location');
+            location_div.innerHTML="<h3>Don't like sharing? Input an address and we'll try to help anyways... TODO</h3>";
         },
         {
-                timeout: (5 * 1000),
-                maximumAge: (1000 * 60 * 15),
-                enableHighAccuracy: true
+            timeout: (5 * 1000),
+            maximumAge: (1000 * 60 * 15),
+            enableHighAccuracy: true
         }
     );
 }
