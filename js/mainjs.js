a9a1c2d4819f6f4ac52b68a09783dd0087cc5fbf
Khulan Myagmardorj <myagmar2@illinois.edu>
2014-11-09 20:25:01 -0600

    change text on hover for price distance and type

--- js/mainjs.js
+++ js/mainjs.js
@@ -1,12 +1,31 @@
 function hover(obj){
   var id = obj.id;
   if (id == "price"){
-
+        document.getElementById("priceok").style.display = "none";
+        document.getElementById("pricebad").style.display = "block";
   }
   else if (id=="distance"){
-
+        document.getElementById("distanceok").style.display = "none";
+        document.getElementById("distancebad").style.display = "block";
   }
   else{
-    
+        document.getElementById("typeok").style.display = "none";
+        document.getElementById("typebad").style.display = "block";
+  }
+}
+
+function hoverout(obj){
+  var id = obj.id;
+  if (id == "price"){
+        document.getElementById("priceok").style.display = "block";
+        document.getElementById("pricebad").style.display = "none";
+  }
+  else if (id=="distance"){
+        document.getElementById("distanceok").style.display = "block";
+        document.getElementById("distancebad").style.display = "none";
+  }
+  else{
+        document.getElementById("typeok").style.display = "block";
+        document.getElementById("typebad").style.display = "none";
   }
 }
