2d5dedca1ca91b06ee3874840ea7c4bdb46e72d8
Akshay Bajaj <akshaybajaj2280@gmail.com>
2014-11-10 03:12:56 -0600

    filters need to be adjusted and updated over time

--- js/mainjs.js
+++ js/mainjs.js
@@ -9,9 +9,12 @@ var distFilter = 5;
 var typeFilter = new Array(0);
 var myLatlng = null;
 var tempDistance = -1;
+var idx=0;
+var all_restaurants = null;
 
 function displayRestaurant(all_restaurants, index) {
     // run filters
+    console.log("on index " + index);
     var dest = all_restaurants[index]['name'] + ", " + all_restaurants[index]['location'].display_address.toString();
     $.when(priceFilterFunct(all_restaurants[index].url), calcDistancePHP(all_restaurants[index].location.coordinate.latitude, all_restaurants[index].location.coordinate.longitude)).done(function(a1, a2){
 
@@ -47,11 +50,11 @@ function displayRestaurant(all_restaurants, index) {
             zoom: 11,
             disableDefaultUI: true
           };
-          $("#restaurant-name").append(all_restaurants[index]['name'].toUpperCase());
+          $("#restaurant-name").html(all_restaurants[index]['name'].toUpperCase());
           map = new google.maps.Map(document.getElementById('map-canvas'),
               mapOptions);
           directionsDisplay.setMap(map);
-          createLegend(myLatlng);
+          // createLegend(myLatlng);
 
       } else {
         // console.log("one of these are not eligible");
@@ -84,7 +87,7 @@ function generateDestinations(latitude, longitude) {
                 $.get('yelp/yelp_api.php?location=' + user_location, function(data) {
                     all_restaurants = jQuery.parseJSON( data );
                     myLatlng = new google.maps.LatLng(latitude,longitude);
-                    displayRestaurant(all_restaurants, 2);
+                    displayRestaurant(all_restaurants, idx);
                 }); // end function
             }
         );
@@ -110,25 +113,25 @@ function initialize() {
     var all_restaurants = generateDestinations(latitude, longitude);
 }
 
-function createLegend(latlng){
-  geocoder.geocode({'latLng': latlng}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      if (results[1]) {
-        var address = results[1].formatted_address;
-        var legend = document.getElementById('legend');
-        var div = document.createElement('div');
-        div.innerHTML = address;
-        legend.appendChild(div);
-        map.controls[google.maps.ControlPosition.RIGHT_TOP].push(document.getElementById('legend'));
-      } else {
-        alert('No results found');
-      }
-    } else {
-      alert('Geocoder failed due to: ' + status);
-    }
-  });
+// function createLegend(latlng){
+//   geocoder.geocode({'latLng': latlng}, function(results, status) {
+//     if (status == google.maps.GeocoderStatus.OK) {
+//       if (results[1]) {
+//         var address = results[1].formatted_address;
+//         var legend = document.getElementById('legend');
+//         var div = document.createElement('div');
+//         div.innerHTML = address;
+//         legend.appendChild(div);
+//         map.controls[google.maps.ControlPosition.RIGHT_TOP].push(document.getElementById('legend'));
+//       } else {
+//         alert('No results found');
+//       }
+//     } else {
+//       alert('Geocoder failed due to: ' + status);
+//     }
+//   });
 
-}
+// }
 
 function calcRoute(start, end) {
     var request = {
@@ -243,6 +246,7 @@ function block(obj){
 
         document.getElementById("priceok").style.display = "none";
         document.getElementById("pricebad").style.display = "block";
+
   }
   else if (id=="distance" && distanceclicked===0){
         distanceclicked=1;
@@ -277,9 +281,13 @@ function block(obj){
         document.getElementById("blocklist").appendChild(newli);
   }
   else{
+    idx++;
+    displayRestaurant(all_restaurants, idx);
     return;
   }
-  
+  idx++;
+  displayRestaurant(all_restaurants, idx);
+
 }
 
 function maybe(){
@@ -293,6 +301,7 @@ function maybe(){
   newli.innerHTML = html;
   document.getElementById("maybelist").appendChild(newli);
   counter++;
+  displayRestaurant(all_restaurants, idx++);
 }
 
 function removeFromMaybe(obj){
