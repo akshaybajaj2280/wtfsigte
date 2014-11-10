b8853fef0c93194efddf0d989a0fb41948981d6a
Khulan Myagmardorj <myagmar2@illinois.edu>
2014-11-09 18:59:43 -0600

    formatting main page

--- js/map.js
+++ js/map.js
@@ -1,8 +1,10 @@
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
+var geocoder;
 
 function initialize() {
+    geocoder = new google.maps.Geocoder();
     directionsDisplay = new google.maps.DirectionsRenderer();
     var latitude = parseFloat(getParam("lat"));
     var longitude = parseFloat(getParam("lon"));
@@ -12,11 +14,33 @@ function initialize() {
     
     var mapOptions = {
       center: myLatlng,
-      zoom: 11
+      zoom: 11,
+      disableDefaultUI: true
     };
     map = new google.maps.Map(document.getElementById('map-canvas'),
         mapOptions);
     directionsDisplay.setMap(map);
+    createLegend(myLatlng);
+}
+
+function createLegend(latlng){
+  geocoder.geocode({'latLng': latlng}, function(results, status) {
+    if (status == google.maps.GeocoderStatus.OK) {
+      if (results[1]) {
+        var address = results[1].formatted_address;
+        var legend = document.getElementById('legend');
+        var div = document.createElement('div');
+        div.innerHTML = address;
+        legend.appendChild(div);
+        map.controls[google.maps.ControlPosition.RIGHT_TOP].push(document.getElementById('legend'));
+      } else {
+        alert('No results found');
+      }
+    } else {
+      alert('Geocoder failed due to: ' + status);
+    }
+  });
+
 }
 
 function calcRoute(start, end) {
